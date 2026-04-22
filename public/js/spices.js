124f7727fef395b15d385ab71e2de4b669ff3162
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-22 02:20:49 -0400

    Update spices.js

--- public/js/spices.js
+++ public/js/spices.js
@@ -3,13 +3,23 @@ import HERB_BLENDS from "./spices/herbBlends.js";
 import SEASONING_MIXES from "./spices/seasoningMixes.js";
 import CULTURAL_SPICES from "./spices/culturalSpices.js";
 import HOT_SPICES from "./spices/hotSpices.js";
+import AROMATIC_SPICES from "./spices/aromatics.js";
+import SEED_SPICES from "./spices/seedSpices.js";
+import BARK_SPICES from "./spices/barkSpices.js";
+import ROOT_SPICES from "./spices/rootSpices.js";
+import FLOWER_SPICES from "./spices/flowerSpices.js";
 
 const ALL_SPICES = [
   ...DRY_SPICES,
   ...HERB_BLENDS,
   ...SEASONING_MIXES,
   ...CULTURAL_SPICES,
-  ...HOT_SPICES
+  ...HOT_SPICES,
+  ...AROMATIC_SPICES,
+  ...SEED_SPICES,
+  ...BARK_SPICES,
+  ...ROOT_SPICES,
+  ...FLOWER_SPICES
 ];
 
 export function getAllSpices() {
