036c181783319cabd27c5f92c97ab2c7ac1b2205
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-22 02:16:35 -0400

    Create spices.js

---
+++ public/js/spices.js
@@ -0,0 +1,27 @@
+import DRY_SPICES from "./spices/drySpices.js";
+import HERB_BLENDS from "./spices/herbBlends.js";
+import SEASONING_MIXES from "./spices/seasoningMixes.js";
+import CULTURAL_SPICES from "./spices/culturalSpices.js";
+import HOT_SPICES from "./spices/hotSpices.js";
+
+const ALL_SPICES = [
+  ...DRY_SPICES,
+  ...HERB_BLENDS,
+  ...SEASONING_MIXES,
+  ...CULTURAL_SPICES,
+  ...HOT_SPICES
+];
+
+export function getAllSpices() {
+  return ALL_SPICES;
+}
+
+export function getSpiceById(id) {
+  return ALL_SPICES.find(s => s.id === id);
+}
+
+export function getSpicesByCategory(category) {
+  return ALL_SPICES.filter(s => s.category === category);
+}
+
+export default ALL_SPICES;
