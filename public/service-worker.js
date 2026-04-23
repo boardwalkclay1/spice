00550888507399352441c33e083742af922617fc
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-23 17:52:08 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "spice-lab-v1";
+
+const ASSETS = [
+  "index.html",
+  "styles.css",
+  "app.js",
+  "data.spices.js",
+  "data.sauces.js",
+  "manifest.json",
+  "icons/favicon.png",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return (
+        cached ||
+        fetch(event.request).catch(() =>
+          caches.match("index.html")
+        )
+      );
+    })
+  );
+});
