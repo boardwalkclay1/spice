d358b401b8edc927be09d24367d6acdac68def8e
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-24 14:50:55 -0400

    Update spices.js

--- public/js/spices.js
+++ public/js/spices.js
@@ -1,25 +1,36 @@
 import DRY_SPICES from "/js/spices/drySpices.js";
+import DRY_SPICES_2 from "/js/spices/drySpices2.js";
+import DRY_SPICES_3 from "/js/spices/drySpices3.js";
+import DRY_SPICES_4 from "/js/spices/drySpices4.js";
+import DRY_SPICES_5 from "/js/spices/drySpices5.js";
+
 import HERB_BLENDS from "/js/spices/herbBlends.js";
 import SEASONING_MIXES from "/js/spices/seasoningMixes.js";
 import CULTURAL_SPICES from "/js/spices/culturalSpices.js";
 import HOT_SPICES from "/js/spices/hotSpices.js";
-import AROMATIC_SPICES from "/js/spices/aromatics.js";
-import SEED_SPICES from "/js/spices/seedSpices.js";
-import BARK_SPICES from "/js/spices/barkSpices.js";
-import ROOT_SPICES from "/js/spices/rootSpices.js";
-import FLOWER_SPICES from "/js/spices/flowerSpices.js";
+
+// You have an Aromatics folder, not a file named aromatics.js
+// So the correct import is:
+import AROMATICS from "/js/spices/Aromatics/Aromatics.js";
+
+// These files DO NOT exist in your repo:
+// barkSpices.js
+// rootSpices.js
+// flowerSpices.js
+// So we REMOVE them completely.
 
 const ALL_SPICES = [
   ...DRY_SPICES,
+  ...DRY_SPICES_2,
+  ...DRY_SPICES_3,
+  ...DRY_SPICES_4,
+  ...DRY_SPICES_5,
+
   ...HERB_BLENDS,
   ...SEASONING_MIXES,
   ...CULTURAL_SPICES,
   ...HOT_SPICES,
-  ...AROMATIC_SPICES,
-  ...SEED_SPICES,
-  ...BARK_SPICES,
-  ...ROOT_SPICES,
-  ...FLOWER_SPICES
+  ...AROMATICS
 ];
 
 export function getAllSpices() {
